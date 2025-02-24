5559d2e99b3dccda67399156946678dac537f29f
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-02-24 17:33:26 +0200

    Added backdrop effect

--- src/app/[locale]/components/sticky-bar.tsx
+++ src/app/[locale]/components/sticky-bar.tsx
@@ -8,22 +8,26 @@ type Props = {
   children: ReactNode;
   heightInRems?: number;
   className?: string;
+  containerClassName?: string;
 };
 
-function StickyBar({ isStickyTop, children, heightInRems = consts.STICKY_BAR_HEIGHT_IN_REMS, className = "" }: Props) {
+function StickyBar({
+  isStickyTop,
+  children,
+  heightInRems = consts.STICKY_BAR_HEIGHT_IN_REMS,
+  className = "",
+  containerClassName = "",
+}: Props) {
   return (
     <div
       style={{
         ...(isStickyTop && { paddingTop: heightInRems + "rem" }),
         ...(!isStickyTop && { paddingBottom: heightInRems + "rem" }),
       }}
+      className={containerClassName}
     >
       <div
-        className={cn(
-          "fixed left-0 right-0 bg-white/90 backdrop-blur z-10",
-          isStickyTop ? "top-0" : "bottom-0",
-          className
-        )}
+        className={cn("fixed left-0 right-0 bg-white z-10", isStickyTop ? "top-0" : "bottom-0", className)}
         style={{ height: heightInRems + "rem" }}
       >
         <Container className="!py-0">
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Container from "./components/container";
 import LetsTalkBusiness from "./components/lets-talk-business";
 import { Locale } from "@/types/locale";
