96e91f29e387395c84cdb860d08cdea9942a5ec2
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-02-28 11:51:52 +0200

    Finished experise page

---
+++ src/app/[locale]/expertise/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/components/container";
+import { getDictionary } from "@/lib/dictionaries";
+import { Locale } from "@/types/locale";
+import React from "react";
+import LetsTalkBusiness from "../components/lets-talk-business";
+import ExpertiseAccordion from "./components/expertise-accordion";
+
+async function ExpertisePage({ params }: { params: Promise<{ locale: Locale }> }) {
+  const { locale } = await params;
+  const { "expertise-page": t } = await getDictionary(locale);
+
+  return (
+    <div className="bg-tggrey">
+      <Container>
+        <LetsTalkBusiness t={t["lets-talk-business"]} />
+      </Container>
+
+      <Container>
+        <ExpertiseAccordion t={t.accordion} locale={locale} />
+      </Container>
+    </div>
+  );
+}
+
+export default ExpertisePage;
